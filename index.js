bea1e8d00d426acc91fe56da543f384dad695641
RelancioBorgesDev <relancio.dev@gmail.com>
2022-03-25 11:16:59 -0300

    Add CRUD

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const http = require('http');
+import app from './src/app.js'
 
-const rota = {
-    '/': 'Curso Node',
-}
-const server = http.createServer((req,res) => {
-    res.writeHead('200', {'Content-Type': 'text/plain'});
-    res.end(rota[req.url])
-    console.log(req.url)
-})
+const port = process.env.PORT || 3000
 
-server.listen(3000, () => {
-    console.log('Working, no BS')
+app.listen(port, () => {
+    console.log(`Servido funcionando em http://localhost:${port}`)
 })
