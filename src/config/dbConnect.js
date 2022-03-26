e3b20717c42745fd27c452c4381eca0862ab9340
RelancioBorgesDev <relancio.dev@gmail.com>
2022-03-26 16:49:01 -0300

    Refatoracao/MVC/Rotas com Router

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,4 +1,8 @@
 //4:00
 import mongoose from "mongoose"
 
-mongoose.connect("")
+mongoose.connect("mongodb+srv://Relnc:123@cluster0.p5nij.mongodb.net/alura-node");
+
+let db = mongoose.connection;
+
+export default db
---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import livrosController from '../controllers/livrosController.js';
+
+const router = express.Router();
+
+router
+    .get("/livros", livrosController.listarLivros)
+    .get("/livros/:id", livrosController.listarLivroPorId)
+    .post("/livros", livrosController.cadastrarLivros)
+    .put("/livros/:id", livrosController.atualizarLivro)
+    .delete("/livros:id", livrosController.excluirLivro)
+export default router 
